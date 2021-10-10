54ccb86d046b6f26002e16593234c10363dd7030
Maria Kuchinskaya <mariakuchinskaya16@gmail.com>
2021-10-10 19:33:40 +0300

    Task2

---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pi = 3.141592653;
+            var e = 1.414213562373095048M;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+        }
+    }
+}
